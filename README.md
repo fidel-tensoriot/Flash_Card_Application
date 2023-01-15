279fe3ac56fdafcec1c774a86b0136bf47a606a2
Fidel Coronel <fidel.coronel@tensoriot.com>
2023-01-15 10:42:57 -0500

    updates

---
+++ README.md
@@ -0,0 +1,32 @@
+# Python FLash Card Application
+
+Written using Python,
+
+## Deployment
+Create a virtualenv and install dependencies. 
+```
+<!-- Install virtualenv library using pip -->
+pip install virtualenv
+
+<!-- Run command to initialize a virtual environment named venv -->
+virtualenv venv
+
+
+<!-- Linux or Mac activate venv  -->
+source venv/bin/activate
+<!-- Windows activate venve -->
+venv/Scripts/activate
+
+<!-- Install dependencies using pip and requirements.txt -->
+pip install -r requirements.txt
+```
+
+## Problems ran into 
+
+https://stackoverflow.com/questions/47357090/tkinter-error-couldnt-recognize-data-in-image-file
+'''
+_tkinter.TclError: couldn't recognize data in image file
+'''
+
+
+https://stackoverflow.com/questions/27180497/typeerror-str-returned-non-string-type-instance
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 from tkinter import *
 import pandas
+from PIL import ImageTk, Image
 
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
@@ -43,18 +44,19 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="Images/card_front.png")
-card_back_img = PhotoImage(file="images/card_back.png")
+
+card_front_img = ImageTk.PhotoImage(file="Images/card_front.png")
+card_back_img = ImageTk.PhotoImage(file="Images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Aerial", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Aerial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
-cross_image = PhotoImage(file="images/wrong.png")
+cross_image = ImageTk.PhotoImage(file="Images/wrong.png")
 unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
-check_image = PhotoImage(file="images/right.png")
+check_image = ImageTk.PhotoImage(file="Images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
